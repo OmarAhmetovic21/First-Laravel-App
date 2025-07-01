12ecf1c65d4e28643b06e8707f9e2e235bdd36f1
OmarAhmetovic21 <92754632+OmarAhmetovic21@users.noreply.github.com>
2025-07-01 17:00:28 +0200

    Basic work

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Početna stranica</title>
+</head>
+<body>
+    <h1>Dobrodošao na Home komponentu u Laravelu!</h1>
+
+        <a href="/welcome">
+        <button>Povratak na Welcome</button>
+    </a>
+</body>
+</html>
